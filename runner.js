e16e5b70a1a50e8074bee047ca982b7a1e35615c
deanlefor <web@le4.me>
2025-07-29 19:53:38 -0400

    Update runner.js
    Updating mobile view

--- runner.js
+++ runner.js
@@ -1,5 +1,8 @@
 // runner.js
 window.onload = () => {
+  // FIX: Call the viewport handler to manage mobile screen height and
+  // ensure the terminal is always the correct size.
+  handleViewportChanges();
   runBootSplash();
   attachInputHandlers();
 };
