6a20dad577c9af82740a734e46ad8ea6d1cd136b
deanlefor <web@le4.me>
2025-07-29 20:17:56 -0400

    Update filesystem.js
    First line break issues

--- filesystem.js
+++ filesystem.js
@@ -46,35 +46,35 @@ window.fs = {
     folders: [],
     files: {
       "DAIGM.TXT":
-        "DEPUTY ASSISTANT INSPECTOR GENERAL FOR MANAGEMENT (2025–Present)\\n" +
-        "NASA Office of Inspector General – Washington, DC\\n" +
-        "\\n" +
-        "• Provides executive oversight for NASA OIG’s administrative operations, including budget, IT, human capital, facilities, and internal policy.\\n" +
-        "• Leads a cross-functional management team, driving strategic alignment between mission support services and OIG oversight priorities.\\n" +
-        "• Oversees execution and compliance of the agency’s multi-million dollar budget while ensuring efficient resource stewardship.\\n" +
-        "• Directs enterprise modernization efforts in information technology and workforce development to enhance operational effectiveness.\\n" +
+        "DEPUTY ASSISTANT INSPECTOR GENERAL FOR MANAGEMENT (2025–Present)\n" +
+        "NASA Office of Inspector General – Washington, DC\n" +
+        "\n" +
+        "• Provides executive oversight for NASA OIG’s administrative operations, including budget, IT, human capital, facilities, and internal policy.\n" +
+        "• Leads a cross-functional management team, driving strategic alignment between mission support services and OIG oversight priorities.\n" +
+        "• Oversees execution and compliance of the agency’s multi-million dollar budget while ensuring efficient resource stewardship.\n" +
+        "• Directs enterprise modernization efforts in information technology and workforce development to enhance operational effectiveness.\n" +
         "• Serves as a senior advisor to the Inspector General and executive leadership, guiding agency-wide initiatives and long-term planning.",
       "RMD_DIRECTOR.TXT":
-        "DIRECTOR, RESOURCES MANAGEMENT DIVISION (2024-2025)\\n" +
-        "NASA Office of Inspector General - Washington, DC\\n" +
-        "\\n" +
-        "• Oversees the strategic direction and financial oversight of NASA OIG’s $47.6M budget, including procurement, policy, and resource allocation.\\n" +
-        "• Directs an 8-member team, establishing performance metrics and professional development to achieve operational excellence.\\n" +
-        "• Leads financial planning and procurement operations for over $9M in federal procurements, ensuring regulatory compliance and cost-effectiveness.\\n" +
-        "• Provides strategic leadership for the OIG's travel and procurement training programs to ensure adherence to federal policies.\\n" +
+        "DIRECTOR, RESOURCES MANAGEMENT DIVISION (2024-2025)\n" +
+        "NASA Office of Inspector General - Washington, DC\n" +
+        "\n" +
+        "• Oversees the strategic direction and financial oversight of NASA OIG’s $47.6M budget, including procurement, policy, and resource allocation.\n" +
+        "• Directs an 8-member team, establishing performance metrics and professional development to achieve operational excellence.\n" +
+        "• Leads financial planning and procurement operations for over $9M in federal procurements, ensuring regulatory compliance and cost-effectiveness.\n" +
+        "• Provides strategic leadership for the OIG's travel and procurement training programs to ensure adherence to federal policies.\n" +
         "• Acts as the primary liaison between NASA OIG Headquarters and its facilities, facilitating collaboration with senior leadership to optimize performance.",
       "PPA_MANAGER.TXT":
-        "MANAGER, PROCUREMENT, POLICY, and ADMINISTRATION\\n" +
-        "NASA Office of Inspector General - Washington, DC\\n" +
-        "\\n" +
-        "• Leads procurement, budgeting, and operations, developing strategies to optimize resource use and ensure regulatory compliance.\\n" +
-        "• Acts as the primary liaison between headquarters and ten nationwide OIG facilities, ensuring seamless communication and continuity of operations.\\n" +
-        "• Directs the coordination of NASA OIG’s Strategic Plan, developing and tracking key performance metrics for internal and Congressional reporting.\\n" +
-        "• Chaired the inaugural ‘Return To Work’ committee and contributed to NASA’s ‘Future of Work’ initiatives, shaping strategic workforce planning.\\n" +
+        "MANAGER, PROCUREMENT, POLICY, and ADMINISTRATION\n" +
+        "NASA Office of Inspector General - Washington, DC\n" +
+        "\n" +
+        "• Leads procurement, budgeting, and operations, developing strategies to optimize resource use and ensure regulatory compliance.\n" +
+        "• Acts as the primary liaison between headquarters and ten nationwide OIG facilities, ensuring seamless communication and continuity of operations.\n" +
+        "• Directs the coordination of NASA OIG’s Strategic Plan, developing and tracking key performance metrics for internal and Congressional reporting.\n" +
+        "• Chaired the inaugural ‘Return To Work’ committee and contributed to NASA’s ‘Future of Work’ initiatives, shaping strategic workforce planning.\n" +
         "• Oversees a diverse portfolio of projects, driving process innovation and cross-divisional collaboration using technologies like PowerBI.",
       "PRIOR.TXT":
-        "• National Aeronautics and Space Administration (NASA), Office of Inspector General (OIG), Operations Officer (2013-2016)\\n" +
-        "• Department of Education, Federal Student Aid, Management Analyst (2008-2013)\\n" +
+        "• National Aeronautics and Space Administration (NASA), Office of Inspector General (OIG), Operations Officer (2013-2016)\n" +
+        "• Department of Education, Federal Student Aid, Management Analyst (2008-2013)\n" +
         "• United States Senate, Budget Committee, Committee Intern (2007)"
     }
   },
